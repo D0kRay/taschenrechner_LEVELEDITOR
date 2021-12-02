2a695d87072ae379c5008e4a27409d4e7ea0dad5
D0kRay <pasi.k11@web.de>
2021-12-02 10:45:18 +0100

    erste Schritte

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class HelloWorld {
     private JButton halloButton;
     private JPanel mainPanel;
     private JTextField nameField;
+    private JLabel anweisungLabel;
 
     public HelloWorld() {
         halloButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(halloButton, "Herzlich Willkommen!");
                 String name = nameField.getText();
                 System.out.println(name);
+//                JOptionPane.showMessageDialog(halloButton, "Herzlich Willkommen!");
+                JOptionPane.showMessageDialog(halloButton, "Hallo " + name);
+            }
+        });
+        nameField.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                Color mintgruen = Color.decode("#ade3d1");
+                nameField.setBackground(mintgruen);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                nameField.setBackground(null);
             }
         });
     }
